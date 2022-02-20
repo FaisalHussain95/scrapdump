fdd1c5297651c5c720d0334a4aa309881cf3607b
Faisal HUSSAIN <faisal.hussain@outlook.fr>
2022-02-20 22:54:55 +0100

    Cotizup - Error handling

--- plugins/cotizup.js
+++ plugins/cotizup.js
@@ -4,60 +4,65 @@ const { http, output } = require('./base');
 const type = 'cotizup';
 
 exports.output = async (params) => {
-  const res = {};
+  const res = {
+    ...output(type),
+    error: false,
+  };
 
-  const pageResponse = await http.get(`https://cotizup.com/${params.id}`);
+  try {
+    const pageResponse = await http.get(`https://cotizup.com/${params.id}`);
 
-  if (pageResponse && pageResponse.data) {
-    const $ = cheerio.load(pageResponse.data);
+    if (pageResponse && pageResponse.data) {
+      const $ = cheerio.load(pageResponse.data);
 
-    /**
+      /**
      * Title
      */
-    const title = $('.campaign-title h2');
+      const title = $('.campaign-title h2');
+      if (title && title.length) {
+        res.title = {
+          raw: title.first().html(),
+          text: title.first().text(),
+        };
+      }
 
-    if (title && title.length) {
-      res.title = {
-        raw: title.first().html(),
-        text: title.first().text(),
-      };
-    }
-
-    /**
+      /**
      * Description
      */
-    const desc = $('.campaign-description');
+      const desc = $('.campaign-description');
+      if (desc && desc.length) {
+        res.desc = {
+          raw: desc.first().html(),
+          text: desc.first().text(),
+        };
+      }
 
-    if (desc && desc.length) {
-      res.desc = {
-        raw: desc.first().html(),
-        text: desc.first().text(),
-      };
-    }
-
-    /**
+      /**
      * Prices
      */
-    const prices = $('.price-total');
+      const prices = $('.price-total');
+      if (prices && prices.length) {
+        res.prices = [];
 
-    if (prices && prices.length) {
-      res.prices = [];
+        prices.each((i, _price) => {
+          const price = $(_price);
 
-      prices.each((i, _price) => {
-        const price = $(_price);
-
-        res.prices.push({
-          raw: price.text(),
-          total: parseInt(price
-            .find('span').text()
-            .replace(/[€$£]/, '')
-            .replace(' ', '')
-            .trim(), 10),
-          collected: parseInt(price.attr('data-collected'), 10),
+          res.prices.push({
+            raw: price.text(),
+            total: parseInt(price
+              .find('span').text()
+              .replace(/[€$£]/, '')
+              .replace(' ', '')
+              .trim(), 10),
+            collected: parseInt(price.attr('data-collected'), 10),
+          });
         });
-      });
+      }
     }
+  } catch (error) {
+    res.error = true;
+    res.message = error.message;
   }
 
-  return { ...output(type), ...res };
+  return res;
 };
